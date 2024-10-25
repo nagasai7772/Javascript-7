8c0ca6025b90be34fece577c330d811339aa8a22
nagasai7772 <nagunagasai@gmail.com>
2024-10-25 16:25:15 +0530

    if else prompt and alert

---
+++ index.js
@@ -0,0 +1,105 @@
+// var num=5
+// if(num%2!=0)
+// {
+//     console.log(`${num} It is Odd Number`);
+// }
+
+//  if(num%2==0)
+// {
+//     console.log(`${num} It is Even Number`);
+// }
+
+// var num1=+prompt("Enter num1:");
+// var num2=+prompt("Enter num2:");
+// if(num1==num2)
+// {
+//     console.log(alert(`Number:${num1},Number:${num2} are Equal`));
+// }
+
+// else if(num1>num2)
+// {
+//     console.log(alert(`Number:${num1} is Greater than Number:${num2}`));
+// }
+// else if(num1<num2)
+// {
+//     console.log(alert(`Number:${num1} is Smaller than Number:${num2}`));
+// }
+// else if(num2>num1)
+// {
+//     console.log(alert(`Number:${num2} is Greater than Number:${num1}`));
+// }
+// else if(num2<num1)
+// {
+//     console.log(alert(`Number:${num2} is Smaller than Number:${num1}`));
+// }
+// else{
+//     console.log(alert("I'm Sorry you entered Wrong input"));
+
+// }
+// var Age = +prompt("Enter Age: ");
+// var Eligible = prompt("Do You valid License");
+// if (Age >= 18) {
+//   console.log(alert("Can Drive"));
+
+//   if (Eligible == true) {
+//     console.log("you can drive");
+//   } else {
+//     cosnole.log("You cannot Drive");
+//   }
+// } else {
+//   console.log("Not Eliglible");
+// }
+
+// var num = +prompt("Enter a number :");
+
+// if (num > 0) {
+//   console.log(alert(`${num} is a positive integer`));
+// } else if (num < 0) {
+//   console.log(alert(`${num} is a Negative number`));
+// } else if (num == 0) {
+//   console.log(alert(`${num}  is Zero`));
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var a = 10;
+// var b = 20;
+// var c = 30;
+// if (a > b && a > c) {
+//   console.log("a is big");
+// } else if (b > a && b > c) {
+//   console.log("b is big");
+// } else if (c > a && a > c) {
+//   console.log("C is big");
+// }
